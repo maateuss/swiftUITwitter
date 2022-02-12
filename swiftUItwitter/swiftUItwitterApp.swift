9d378b142c5ca2e08148570448c0cfccea12a7c0
Mateus Moura Santos <mateus.moura.santos@santander.com.br>
2022-02-12 09:35:46 -0300

    Initial Commit

---
+++ swiftUItwitter/swiftUItwitterApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUItwitterApp.swift
+//  swiftUItwitter
+//
+//  Created by Mateus Moura Santos on 12/02/22.
+//
+
+import SwiftUI
+
+@main
+struct swiftUItwitterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
